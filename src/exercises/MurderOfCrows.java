a582d4ed8f7ceb4b33de2b7af0a2017cffd8d2f4
Barezi79 <branislavnesic79@gmail.com>
2019-06-07 00:19:57 +0200

    Exercise MurderOfCrows

--- src/exercises/MurderOfCrows.java
+++ src/exercises/MurderOfCrows.java
@@ -1,5 +1,7 @@
 package exercises;
+
 import java.util.ArrayList;
+
 import java.util.Random;
 
 public class MurderOfCrows {
@@ -14,12 +16,26 @@ public class MurderOfCrows {
 
 	private void findTheDiamond() {
 		/*
-		 * 1. One of the Crows has eaten the diamond. You need to search through the stomach of each Crow, 
-		 * then print the name of the guilty Crow.
+		 * 1. One of the Crows has eaten the diamond. You need to search through the
+		 * stomach of each Crow, then print the name of the guilty Crow.
 		 */
-		
+
 		/* 2. How many innocent crows had to die before the diamond was found? */
-		
+
+		int innocentCrowsKilled = 0;
+		for (int i = 0; i < theMurder.size(); i++)	{
+
+			if (theMurder.get(i).getStomachContents().contains("diamond")) {
+
+				System.out.println(theMurder.get(i).getName() + " has eaten the diamond.");
+				break;
+
+			} else {
+				innocentCrowsKilled++;
+			}
+		}
+		System.out.println(innocentCrowsKilled + " innocent crows had to die before the diamond was found.");
+
 	}
 
 	private void initializeCrows() {
@@ -53,11 +69,16 @@ class Crow {
 
 	private String getRandomCrowFood() {
 		int randomness = new Random().nextInt(4);
-		if (randomness == 0) return "carrion";
-		else if (randomness == 1)return "snail";
-		else if (randomness == 2)return "acorn";
-		else if (randomness == 3)return "spider";
-		else return "grub";
+		if (randomness == 0)
+			return "carrion";
+		else if (randomness == 1)
+			return "snail";
+		else if (randomness == 2)
+			return "acorn";
+		else if (randomness == 3)
+			return "spider";
+		else
+			return "grub";
 	}
 
 	public String getName() {
@@ -69,5 +90,3 @@ class Crow {
 	}
 
 }
-
-
