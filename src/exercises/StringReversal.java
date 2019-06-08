2117e40a9f4a69d072440375e138aa0476e8f039
Barezi79 <branislavnesic79@gmail.com>
2019-06-08 20:29:22 +0200

    Exercise StringReversal

--- src/exercises/StringReversal.java
+++ src/exercises/StringReversal.java
@@ -11,8 +11,27 @@ public class StringReversal extends TestCase {
 	 *       QcXgW9w4wQd=v?hctaw/moc.ebutuoy
 	 */
 
-//	public void test() {
-//		assertEquals("cellardoor", reverseMe("roodrallec"));
-//	}
+	public void test() {
+		assertEquals("cellardoor", reverseMe("roodrallec"));
+		System.out.println(reverseMe("QcXgW9w4wQd=v?hctaw/moc.ebutuoy"));
+	}
 
+//	private String reverseMe(String makeReverse) {
+//		String reverse = "";
+//		
+//		for (int i = makeReverse.length() - 1; i>= 0; i--) {
+//			reverse += makeReverse.charAt(i);
+//		}
+//		return reverse;
+//		
+//	}
+	private String reverseMe(String makeReverse) {
+		String reverse = "";
+		char [] ourString = makeReverse.toCharArray();
+		for (int i = makeReverse.length() - 1; i>= 0; i--) {
+			reverse += ourString[i];
+		}
+		return reverse;
+	}
+	
 }
